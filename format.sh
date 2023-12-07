f57e06b6b62b96443e7848295e2fd72efc65fd61
akielaries <akiel@akiel.org>
2023-12-07 01:46:19 -0700

    updates to format

--- format.sh
+++ format.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . -regex '.*\.\(c\|y\|cuh\|cpp\|hpp\|h\|cxx\)' -exec clang-format -style=file -i {} \;
+find . -regex '.*\.\(c\|cu\|cuh\|cpp\|hpp\|h\|cxx\)' -exec clang-format -style=file -i {} \;
